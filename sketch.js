29a503e3de73de5b006063bc6a040a8881bc86ac
jashraj994 <81307288+jashraj994@users.noreply.github.com>
2021-08-09 16:10:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var playerCount,database;
+var canvas,backgroundImg;
+var gameState = 0;
+var form,player,game;
+var allPlayers;
+function setup(){
+
+    database = firebase.database();
+    canvas = createCanvas(500,500);
+
+    game = new Game();
+    game.getState();
+    game.start();
+    
+   
+}
+
+function draw(){
+    if(playerCount===4){
+    game.update(1);
+    }
+    if(gameState===1){
+    clear();
+    game.play();
+    }
+}
